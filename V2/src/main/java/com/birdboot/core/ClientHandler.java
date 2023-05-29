f20e431cb70a087e71bd1586c976e4ae9ae44a7b
nanlingyang <928701244@qq.com>
2023-05-29 14:11:04 +0800

    V2项目创建并更新readme.txt文件

---
+++ V2/src/main/java/com/birdboot/core/ClientHandler.java
@@ -0,0 +1,24 @@
+package com.birdboot.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+    public void run(){
+        try {
+            InputStream in = socket.getInputStream();
+            int d;
+            while((d = in.read())!=-1){
+                System.out.print((char)d);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
